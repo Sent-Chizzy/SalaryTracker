e45ddea63dc156624329824bc55940269d3ea6e6
Godsent Ukomadu <Godsentukomadu@outlook.com>
2023-09-23 18:32:20 +0100

    Add project files.

---
+++ SalaryTracker/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalaryTracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage ="This Field cannot be Empty.")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Type { get; set; } = "Expense";
+        [NotMapped]
+        public string? TitleWithIcon { get 
+            {
+                return this.Icon + " " + this.Title;
+            } }
+    }
+}
